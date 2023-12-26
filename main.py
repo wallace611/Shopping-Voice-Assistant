8ac9eb64f7a705d35f86cd81ae2cea9edb20a1a7
wallace611 <wallacef930814@gmail.com>
2023-12-26 17:21:21 +0800

    import mining module

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-if __name__ == '__main__':
-    print("good")
+from source import module
+
+
+args = {
+    "data file": "test.dat",
+    "minimum support": 0.1,
+    "minimum confidence": 0.8,
+    "limit": 5,
+    "write file": True,
+    "parallel processing": "auto" # "always", "never", others = "auto"
+}
+module.training(args)
+    
---
+++ source/__init__.py
@@ -0,0 +1,1 @@
+from source.asso_rule import module
